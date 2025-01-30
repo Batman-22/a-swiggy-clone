6600eb9683b0a20f8b7df563cf52f95960ed6afd
Batman-22 <jjthomas2212@gmail.com>
2025-01-30 11:19:55 +0530

    updated

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  collectCoverage: true,  // Enable code coverage collection
-  coverageDirectory: 'coverage',  // Directory for coverage reports
-  coverageReporters: ['lcov', 'text-summary'],  // Format of the coverage reports
-  testEnvironment: 'jsdom',  // The environment for React tests
+  collectCoverage: true, // Enable code coverage collection
+  coverageDirectory: 'coverage', // Directory for coverage reports
+  coverageReporters: ['lcov', 'text-summary'], // Format of the coverage reports
+  testEnvironment: 'jsdom', // The environment for React tests
+  testMatch: [
+    '**/test/**/*.test.js', // Look for test files in the test folder
+  ],
 };
 
